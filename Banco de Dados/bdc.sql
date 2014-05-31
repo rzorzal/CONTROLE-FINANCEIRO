7b37a5af9025aec64863bfda1683d4823237ff9a
Ricardo Zorzal Davila <ricardozorzal@hotmail.com>
2014-05-31 12:54:39 -0300

    Foi corrigido alguns erros, e colocado os COMMENT nas tabelas

--- Banco de Dados/bdc.sql
+++ Banco de Dados/bdc.sql
@@ -1,20 +1,20 @@
 CREATE IF NOT EXISTS TABLE USUARIO(
-	ID int not null AUTO_INCREMENT,
-	Nome char(30) not null,
-	Sobrenome char(30),
-	Email char(50) not null,
-	Username char(50) not null,
-	Senha char(50) not null,
+	ID int not null AUTO_INCREMENT COMMENT 'ID do Usuário, pk, autoincrement',
+	Nome char(30) not null COMMENT 'Primeiro nome',
+	Sobrenome char(30) COMMENT 'O resto do nome',
+	Email char(50) not null COMMENT 'E-mail, pk',
+	Username char(50) not null COMMENT 'Nome de usuario, pk',
+	Senha char(50) not null COMMENT 'senha escolhida pelo usuario',
 	
 	CONSTRAINT pk_usuario PRIMARY KEY (Email,Username,ID)
 	
 );
 
 CREATE IF NOT EXISTS TABLE ACESSO(
-	ID int not null AUTO_INCREMENT,
-	ID_Usuario int not null,
-	Data_Acesso date not null,
-	IP char(17),
+	ID int not null AUTO_INCREMENT COMMENT 'ID acesso, pk, autoincrement',
+	ID_Usuario int not null COMMENT 'Id do usuario que acessou, fk',
+	Data_Acesso date not null COMMENT 'Data de acesso, geralmente usa-se sysdate como defaut',
+	IP char(17) COMMENT 'IP de acesso usado pelo usuario,',
 	
 	CONSTRAINT pk_acesso PRIMARY KEY (ID),
 	CONSTRAINT fk_acesso_x_usuario FOREIGN KEY (ID_Usuario) REFERENCES USUARIO(ID)
@@ -22,13 +22,13 @@ CREATE IF NOT EXISTS TABLE ACESSO(
 );
 
 CREATE IF NOT EXISTS TABLE CONTA_A_PAGAR(
-	ID int not null AUTO_INCREMENT,
-	Valor float not null,
-	Status char(15) not null DEFAULT 'Pendente',
-	OBS char(100),
-	Data_Criacao date not null,
-	Data_Pago date,
-	ID_Usuario int not null,
+	ID int not null AUTO_INCREMENT COMMENT 'ID de contas a pagar, pk, autoincrement',
+	Valor float not null COMMENT 'Valor referente a conta',
+	Status char(15) not null DEFAULT 'Pendente' COMMENT 'Status da conta, cc',
+	OBS char(100) COMMENT 'Obs referente a conta',
+	Data_Criacao date not null COMMENT 'Data criação da conta, geralmente usa-se sysdate',
+	Data_Pago date COMMENT 'Quando altera-se a conta para paga, edita-se este campo',
+	ID_Usuario int not null COMMENT 'Id do usuario que possui a conta, FK',
 	
 	CONSTRAINT ck_Status CHECK (Status in ('Pendente','Pago')),
 	CONSTRAINT pk_ID PRIMARY KEY (ID)
@@ -37,13 +37,13 @@ CREATE IF NOT EXISTS TABLE CONTA_A_PAGAR(
 );
 
 CREATE IF NOT EXISTS TABLE CONTA_A_RECEBER(
-	ID int not null AUTO_INCREMENT,
-	Valor float not null,
-	Status char(15) not null DEFAULT 'Pendente',
-	OBS char(100),
-	Data_Criacao date not null,
-	Data_Pago date,
-	ID_Usuario int not null,
+	ID int not null AUTO_INCREMENT COMMENT 'ID de contas a recever, pk, autoincrement',
+	Valor float not null COMMENT 'Valor refetene a conta a receber',
+	Status char(15) not null DEFAULT 'Pendente' COMMENT 'Status da conta, cc',
+	OBS char(100) COMMENT 'Obs refetente a conta',
+	Data_Criacao date not null COMMENT 'Data de criação da conta, geralmente usa-se sysdate',
+	Data_Recebido date COMMENT 'Quando altera-se o status para recebido, altera-se este campo',
+	ID_Usuario int not null COMMENT 'ID do usuario que possui a conta a receber, fk',
 	
 	CONSTRAINT ck_Status CHECK (Status in ('Pendente','Recebido')),
 	CONSTRAINT pk_ID PRIMARY KEY (ID)
